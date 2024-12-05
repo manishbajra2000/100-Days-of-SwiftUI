34c4a985c4153c3140bec88cd48d970e1f1c17e9
manishbajra2000 <mantu.bajra2000@gmail.com>
2024-12-05 22:46:09 +0545

    Added Checkpoint 6 and Day 11

--- Checkpoint 6.playground/Contents.swift
+++ Checkpoint 6.playground/Contents.swift
@@ -1,3 +1,74 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+enum gears {
+    case neutral
+    case first
+    case second
+    case third
+    case fourth
+    case fifth
+    case sixth
+}
+
+struct Car {
+    nonisolated(unsafe) static var myCars: [String] = []
+    nonisolated(unsafe) static var myCarsCount: Int = 0
+    
+    static func printCars() {
+        if myCarsCount > 0 {
+            print("Cars:")
+            myCars.forEach { print($0) }
+        }else {
+            print("No Cars")
+        }
+    }
+    
+    static func printCarsCount() {
+        print("Cars Count: \(myCarsCount)")
+    }
+    
+    private let model: String
+    private let numberOfSeats: Int
+    private var currentGear: gears  = gears.neutral {
+        didSet {
+            if currentGear == oldValue{
+                print("Already in \(currentGear)")
+            }else {
+                print("Gear changed to \(currentGear)")
+            }
+        }
+    }
+    
+    init(model: String, numberOfSeats: Int) {
+        self.model = model
+        self.numberOfSeats = numberOfSeats
+        print("Car Created: \(model) with \(numberOfSeats) seats")
+        Car.myCars.append(model)
+        Car.myCarsCount += 1
+    }
+    
+    mutating func changeGear(to newGear: gears) {
+        currentGear = newGear
+    }
+    
+    func printGear() {
+        print("Current Gear of \(model): \(currentGear)")
+    }
+}
+
+var ford = Car(model: "Ford Raptor", numberOfSeats: 5)
+ford.changeGear(to: .third)
+ford.changeGear(to: .fourth)
+ford.changeGear(to: .neutral)
+
+var tata = Car(model: "Tata Tiago", numberOfSeats: 4)
+tata.changeGear(to: .neutral)
+tata.changeGear(to: .third)
+tata.changeGear(to: .fourth)
+tata.changeGear(to: .neutral)
+
+Car.printCars()
+Car.printCarsCount()
+
+ford.printGear()
+tata.printGear()
--- Day 11.playground/Contents.swift
+++ Day 11.playground/Contents.swift
@@ -1,3 +1,109 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct BankAccount {
+    private(set) var funds: Int = 0
+    
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+    
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        }
+        else {
+            return false
+        }
+    }
+}
+
+var account =  BankAccount()
+account.deposit(amount: 100)
+
+let success = account.withdraw(amount: 50)
+
+if success {
+    print("Withdrawal successful")
+}else {
+    print("Insufficient funds")
+}
+print(account.funds)
+
+//account.funds -= 1000
+
+print(account.funds)
+
+
+//access controls
+/*
+ private                 Don't let anything outside the struct use this.
+ fileprivate             Don't let anything outside the current file use this.
+ public                  Let anyone, anywhere use this.
+ private(set)            Don;t let anything outside write to this, but can read this.
+ */
+
+
+
+//TIP: if you have a private access control value property inside your struct there is a good chance you have to create your own initializer
+
+
+struct SecretAgent {
+    private var actualName: String
+    public var codeName: String
+    init(name: String, codeName: String) {
+        self.actualName = name
+        self.codeName = codeName
+    }
+}
+let bond = SecretAgent(name: "James Bond", codeName: "007")
+print(bond.codeName)
+
+
+struct RebelBase {
+    private var location: String
+    private var peopleCount: Int
+    
+    init(location: String, people: Int) {
+        self.location = location
+        self.peopleCount = people
+    }
+}
+
+let rebels = RebelBase(location: "Galactic Republic", people: 100000)
+
+struct School {
+    var staffNames: [String]
+    private(set) var studentNames: [String]
+    init(staff: [String]) {
+        self.staffNames = staff
+        self.studentNames = ["Mr. Potter"]
+    }
+}
+let royalHigh = School(staff: ["Mrs Hughes"])
+print(royalHigh.studentNames)
+
+struct Toy {
+    var customerPrice: Double
+    private var actualPrice: Int
+    init(price: Int) {
+        actualPrice = price
+        customerPrice = Double(actualPrice) * 2
+    }
+}
+let buzz = Toy(price: 10)
+
+
+struct LittleAngels {
+    nonisolated(unsafe) static var studentCount: Int = 0
+    
+    static func addStudent(student: String) {
+        print("\(student) has joined the Little Angels")
+        studentCount += 1
+    }
+}
+
+LittleAngels.addStudent(student: "Mr. Potter")
+LittleAngels.addStudent(student: "Mrs. Hughes")
+print(LittleAngels.studentCount)
+
